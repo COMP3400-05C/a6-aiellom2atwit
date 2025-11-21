1101270a05146ab967de97e9ecb1194143f0958d
aiellom2atwit <144489694+aiellom2atwit@users.noreply.github.com>
2025-11-21 13:18:01 -0500

    Completed parta.c and parta_main.c with comments

--- parta.c
+++ parta.c
@@ -3,46 +3,158 @@
 #include <stdio.h>
 
 /**
- * TODO: Describe what the function does
+ * This function takes an array of CPU bursts, and returns an array of PCBs
  */
 struct pcb* init_procs(int* bursts, int blen) {
-    return NULL;
+    //if blen is 0 or less, invalid
+    if (blen <= 0) {
+        return NULL;
+    }
+    //create procs array
+    struct pcb* procs = malloc(sizeof(struct pcb) * blen);
+    //if null, return null
+    if (procs == NULL) {
+        return NULL;
+    }
+    //initialize for each burst
+    for (int i = 0; i < blen; i++) {
+        procs[i].pid = i;
+        procs[i].burst_left = bursts[i];
+        procs[i].wait = 0;
+    }
+    return procs;
 }
 
 /**
- * TODO: Describe what the function does
+ * Helper function that prints the current values of each PCB.
  */
 void printall(struct pcb* procs, int plen) {
-    // Feel free to print however you want
+    if (plen <= 0 || procs == NULL) {
+        return;
+    }
+    for (int i = 0; i < plen; i++) {
+        printf("P%d: Burst %d", procs[i].pid, procs[i].burst_left);
+    }
+    return;
 }
 
 /**
- * TODO: Describe what the function does
+ * "runs" the current process by reducing its burst_left by amount,
+ and also increases the other processes wait by the same amount.
  */
 void run_proc(struct pcb* procs, int plen, int current, int amount) {
-    // TODO: Complete and document
+    //if length is 0 or procs is NULL return
+    if (plen <= 0 || procs == NULL) {
+        return;
+    }
+    
+    //set t to lesser amount
+    int t = amount;
+    if (procs[current].burst_left < t) {
+        t = procs[current].burst_left;
+    }
+
+    //subtract burst_left by calculated t
+    procs[current].burst_left -= t;
+
+    //add t to other procs
+    for (int i = 0; i < plen; i++) {
+        if (i != current && procs[i].burst_left > 0) {
+            procs[i].wait += t;
+        }
+    }
+    return;
 }
 
 /**
- * TODO: Describe what the function does
+ * fcfs Scheduler
  */
 int fcfs_run(struct pcb* procs, int plen) {
-    // TODO: Complete and document
-    return -1;
+    if (plen == 0 || procs == NULL) {
+        return -1;
+    }
+
+    int time = 0;
+
+    //cycle through every pcb in procs array
+    for (int i = 0; i < plen; i++) {
+        //run this process until it finishes
+        while (procs[i].burst_left > 0) {
+            //use run_proc for 1 time unit
+            run_proc(procs, plen, i, 1);
+
+            //increase total time elapsed
+            time += 1;
+        }
+    }
+
+    return time;
 }
 
 /**
- * TODO: Describe what the function does
+ * return the next process to run
  */
 int rr_next(int current, struct pcb* procs, int plen) {
-    // TODO: Complete and document
-    return -1;
+    if (plen == 0 || procs == NULL) {
+        return -1;
+    }
+
+    //test if every burst_left is complete
+    bool allDone = true;
+    for (int i = 0; i < plen; i++) {
+        if (procs[i].burst_left > 0) {
+            allDone = false;
+        }
+    }
+    //return -1 if alldone
+    if (allDone) {
+        return -1;
+    }
+
+    //cycle to next pcb until all are complete
+    int next = (current + 1) % plen;
+    while (procs[next].burst_left == 0) {
+        next = (next + 1) % plen;
+    }
+    return next;
 }
 /**
- * TODO: Describe what the function does
+ * rr Scheduler
  */
 int rr_run(struct pcb* procs, int plen, int quantum) {
-    // TODO: Complete and document
-    return -1;
+    if (plen == 0 || procs == NULL || quantum <= 0) {
+        return -1;
+    }
+
+    //set time and current to 0 to initialize
+    int time = 0;
+    int current = 0;
+
+    //keep looping until completed
+    while (rr_next(current, procs, plen) != -1 || procs[current].burst_left != 0) {
+        int t = quantum;
+        //set t to burst_left if less than quantum
+        if (procs[current].burst_left < t) {
+            t = procs[current].burst_left;
+        }
+
+        //use run_proc with current and t
+        run_proc(procs, plen, current, t);
+
+        //add t to time
+        time += t;
+
+        //set next to rr_next value
+        int next = rr_next(current, procs, plen);
+        //if rr_next returns -1, no more values to complete
+        if (next == -1) {
+            break;
+        }
+
+        //iterate to next
+        current = next;
+    }
+
+    return time;
 }
 
--- parta_main.c
+++ parta_main.c
@@ -5,6 +5,94 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
+    if (argc <= 1) {
+        printf("ERROR: Missing arguments\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "rr") == 0) {
+        // Need at least 1 burst after the algorithm name
+        if (argc <= 3) {
+            printf("ERROR: Missing arguments\n");
+            return 1;
+        }
+
+        //set quantum to the int of second arg
+        int quantum = atoi(argv[2]);
+        printf("Using RR(%d)\n\n", quantum);
+
+        int plen = argc - 3;
+        //create array of bursts
+        int* bursts = malloc(sizeof(int) * plen);
+
+
+        //read args into array
+        for (int i = 0; i < plen; i++) {
+            bursts[i] = atoi(argv[i + 3]); //convert to int from string args
+            printf("Accepted P%d: Burst %d\n", i, bursts[i]);
+        }
+
+        struct pcb* procs = init_procs(bursts, plen);
+
+        rr_run(procs, plen, quantum);
+
+        //calculate average wait time
+        double avg = 0;
+        for (int i = 0; i < plen; i++) {
+            avg += procs[i].wait;
+        }
+        avg /= plen;
+
+        //print wait time
+        printf("Average wait time: %.2f\n", avg);
+
+        //free arrays
+        free(bursts);
+        free(procs);
+        return 0;
+    }
+    else if (strcmp(argv[1], "fcfs") == 0) {
+        // Need at least 1 burst after the algorithm name
+        if (argc <= 2) {
+            printf("ERROR: Missing arguments\n");
+            return 1;
+        }
+
+        printf("Using FCFS\n\n");
+
+        int plen = argc - 2;
+        //create array of bursts
+        int* bursts = malloc(sizeof(int) * plen);
+
+
+        //read args into array
+        for (int i = 0; i < plen; i++) {
+            bursts[i] = atoi(argv[i + 2]); //convert to int from string args
+            printf("Accepted P%d: Burst %d\n", i, bursts[i]);
+        }
+
+        //create array of procs from bursts
+        struct pcb* procs = init_procs(bursts, plen);
+
+        //run fcfs
+        fcfs_run(procs, plen);
+
+        //calculate average wait time
+        double avg = 0;
+        for (int i = 0; i < plen; i++) {
+            avg += procs[i].wait;
+        }
+        avg /= plen;
+
+
+        //print wait time
+        printf("Average wait time: %.2f\n", avg);
+
+        //free arrays
+        free(bursts);
+        free(procs);
+        return 0;
+    } else {
+        printf("ERROR: Missing arguments\n");
+    }
     return 0;
 }
